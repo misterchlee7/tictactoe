56b62fcdaae1a7face8841a85c6289063155beea
Chang Lee <cleeucla@gmail.com>
2018-10-12 09:53:44 -0700

    create win condition checker funcs

--- tictactoe.js
+++ tictactoe.js
@@ -1,13 +1,27 @@
-const tictactoe = () => {
-  const matrix = [];
+// install npm module prompt to receive user input
+const prompt = require('prompt');
+const matrix = []
+
+prompt.start();
+
+// boardCreation function to generate new matrix
+const boardCreation = () => {
   const board = () => {
     for (let i = 0; i < 3; i++) {
       matrix.push(new Array(3));
     }
   }
   board();
-  document.prompt('whats your name bitch')
-  console.log(matrix);
 }
+boardCreation();
 
-tictactoe();
+prompt.get(['x_coord', 'y_coord'], (err, result) => {
+  if (err) {
+    throw new Error('cannot do that');
+  } else {
+    console.log('whats your move');
+    console.log(' x coord is: ', result.x_coord)
+    console.log(' y coord is: ', result.y_coord)
+    console.log(matrix);
+  }
+})
