9ef9fed239230b0482e764be1271ec43e87a47b0
Chang Lee <cleeucla@gmail.com>
2018-10-12 09:30:11 -0700

    add npm mod prompt

--- tictactoe.js
+++ tictactoe.js
@@ -3,10 +3,10 @@ const tictactoe = () => {
   const board = () => {
     for (let i = 0; i < 3; i++) {
       matrix.push(new Array(3));
-    } 
+    }
   }
   board();
-
+  document.prompt('whats your name bitch')
   console.log(matrix);
 }
 
