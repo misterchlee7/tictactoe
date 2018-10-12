d49073ed1cd1ffe27bf07c5cd5cbd7e6a9c1149d
Chang Lee <cleeucla@gmail.com>
2018-10-12 11:28:29 -0700

    update win checker helper funcs

--- tictactoe.js
+++ tictactoe.js
@@ -8,20 +8,74 @@ prompt.start();
 const boardCreation = () => {
   const board = () => {
     for (let i = 0; i < 3; i++) {
-      matrix.push(new Array(3));
+      matrix.push(new Array(3).fill(' '));
     }
   }
   board();
 }
 boardCreation();
 
-prompt.get(['x_coord', 'y_coord'], (err, result) => {
-  if (err) {
-    throw new Error('cannot do that');
+// Win condition checkers
+const winCheck = () => {
+  const rowResult = rowCheck(matrix);
+  const colResult = colCheck(matrix);
+  const diagResult = diagCheck(matrix);
+  if (rowResult || colResult || diagResult) {
+    console.log('you win!');
+    return true;
   } else {
-    console.log('whats your move');
-    console.log(' x coord is: ', result.x_coord)
-    console.log(' y coord is: ', result.y_coord)
-    console.log(matrix);
+    return false;
   }
-})
+}
+
+const rowCheck = (matrix) => {
+  if ((matrix[0][0] === matrix[0][1] && matrix[0][0] === matrix[0][2]) && !(matrix[0][0] === ' ' && matrix[0][1] === ' ' && matrix[0][2] === ' ')) {
+    return true;
+  }
+  else if ((matrix[1][0] === matrix[1][1] && matrix[1][0] === matrix[1][2]) && !(matrix[1][0] === ' ' && matrix[1][1] === ' ' && matrix[1][2] === ' ')) {
+    return true;
+  }
+  else if ((matrix[2][0] === matrix[2][1] && matrix[2][0] === matrix[2][2]) && !(matrix[2][0] === ' ' && matrix[2][1] === ' ' && matrix[2][2] === ' ')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const colCheck = (matrix) => {
+  if ((matrix[0][0] === matrix[1][0] && matrix[0][0] === matrix[2][0]) && !(matrix[0][0] === ' ' && matrix[1][0] === ' ' && matrix[2][0] === ' ')) {
+    return true;
+  }
+  else if ((matrix[0][1] === matrix[1][1] && matrix[0][1] === matrix[2][1]) && !(matrix[0][1] === ' ' && matrix[1][1] === ' ' && matrix[2][1] === ' ')) {
+    return true;
+  }
+  else if ((matrix[0][2] === matrix[1][2] && matrix[0][2] === matrix[2][2]) && !(matrix[0][2] === ' ' && matrix[1][2] === ' ' && matrix[2][2] === ' ')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const diagCheck = (matrix) => {
+  if ((matrix[0][0] == matrix[1][1] && matrix[0][0] === matrix[2][2]) && !(matrix[0][0] === ' ' && matrix[1][1] === ' ' && matrix[2][2] === ' ')) {
+    return true;
+  }
+  else if ((matrix[0][2] == matrix[1][1] && matrix[0][2] === matrix[2][0]) && !(matrix[0][2] === ' ' && matrix[1][1] === ' ' && matrix[2][0] === ' ')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// prompt.get(['x_coord', 'y_coord'], (err, result) => {
+//   if (err) {
+//     throw new Error('cannot do that');
+//   } else {
+//     console.log('whats your move');
+//     console.log(' x coord is: ', result.x_coord)
+//     console.log(' y coord is: ', result.y_coord)
+//     console.log(matrix);
+//   }
+// })
+
+console.log(matrix)
